ef36c437c633acab7201ace19ad3ad3d606b4245
Old Time Sailors <inti.t.silva@gmail.com>
2024-04-21 23:33:49 -0300

    Default fix

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 const server = app.listen(PORT, () => {
   console.log(`Server running in port ${PORT}`);
 });
